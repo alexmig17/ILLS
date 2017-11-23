d8d452fc5d97d6ead70f57d650f9096a45554454
MINSK\Aliaksei_Drapun <alexmig17mail@gmil.com>
2017-11-23 19:04:34 +0300

    added datasource

--- webcore/src/main/java/com/ills/controllers/MainController.java
+++ webcore/src/main/java/com/ills/controllers/MainController.java
@@ -1,17 +1,65 @@
 package com.ills.controllers;
 
 
+import com.ills.entities.Person;
+import com.ills.service.SimpleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "**")
+    @RequestMapping(value = "login")
+
     public String loginPage(){
+
+        SimpleService simpleService;
         return "login";
     }
+
+    @RequestMapping("getPerson")
+    @ResponseBody
+    public Person getPerson(){
+        if(true){
+            throw new NullPointerException();
+        }
+        return new Person();
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        JtaTransactionManager txManager = null;
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setName("SomeTxName");//setName
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_MANDATORY);//PROPAGATION_MANDATORY
+        def.setIsolationLevel(TransactionDefinition.ISOLATION_REPEATABLE_READ);//setIsolationLevel
+
+        TransactionStatus status = txManager.getTransaction(def);//getTransaction
+        try {
+// execute your business logic here
+        }
+        catch (Exception ex) {
+            txManager.rollback(status);//rollback
+            throw ex;
+        }
+        txManager.commit(status);//commit
+    }
+
+
+
 
 
 }
